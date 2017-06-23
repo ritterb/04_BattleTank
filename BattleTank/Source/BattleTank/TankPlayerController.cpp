b7d5ca282be9120858feb6322a8d78b6824235e7
Brian Ritter <ritterb@gmail.com>
2017-06-22 22:27:57 -0500

    Add aim and detect hit method stubs

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -15,7 +15,12 @@ void ATankPlayerController::BeginPlay() {
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing tank: %s"), *controlledTank->GetName());
 	}
+}
 
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerController Tick %f"), DeltaTime);
+	AimTowardsCrosshair();
 }
 
 
@@ -24,3 +29,28 @@ ATank* ATankPlayerController::GetControlledTank() const {
 }
 
 
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) {
+		return;
+	}
+
+	FVector hitLocation; // OUT parameter
+	if (GetSightRayHitLocation(hitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *hitLocation.ToString());
+	}
+
+	
+	// get world location of linetrace through crosshair
+	// if it hits the landscape
+		// tell controlled tank to aim at this point
+
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& hitLocation) const {
+	FHitResult hitResult;
+	FVector start;
+	bool bHitActor;
+	bHitActor = GetWorld()->GetFirstPlayerController()->ActorLineTraceSingle(hitResult, GetPawn()->GetActorLocation(), hitLocation, ECollisionChannel::ECC_PhysicsBody, FName());
+	hitLocation = hitResult.Location;
+	return bHitActor;
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,7 +15,14 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	ATank* GetControlledTank() const;
+
+	// start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector& hitLocation) const;
 };
