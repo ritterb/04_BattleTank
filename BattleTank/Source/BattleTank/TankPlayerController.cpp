04701cd6eed50908e4855e8028a0b6bfe86a846c
Brian Ritter <ritterb@gmail.com>
2017-06-23 22:22:45 -0500

    Add line trace from reticle to visible objects in world up to 10 km

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -47,10 +47,65 @@ void ATankPlayerController::AimTowardsCrosshair() {
 }
 
 bool ATankPlayerController::GetSightRayHitLocation(FVector& hitLocation) const {
+
+	// find the crosshair position in pixel coordinates
+	int32 viewportSizeX, viewportSizeY;
+	GetViewportSize(viewportSizeX, viewportSizeY);
+	FVector2D screenLocation = FVector2D(
+		viewportSizeX * crosshairLocationX,
+		viewportSizeY * crosshairLocationY
+	);
+
+	// de-project the screen position of the crosshair to a world direction
+	FVector lookDirection;
+	if (GetLookDirection(screenLocation, lookDirection)) {
+
+		// line-trace along that look direction, and see what we hit (up to max range)
+		//FHitResult hitResult;
+		bool bGotHit = GetLookVectorHitLocation(lookDirection, hitLocation);
+		//	GetPawn()->GetActorLocation() + FVector(0,0,100),
+		//	GetPawn()->GetActorLocation() + lookDirection * lineTraceRange,
+		//	hitResult
+		//);
+		if (bGotHit || true) {
+			UE_LOG(LogTemp, Warning, TEXT("World direction: %s"), *hitLocation.ToString());
+		}
+
+	}
+
+	
+	
+
+	//bHitActor = GetWorld()->GetFirstPlayerController()->ActorLineTraceSingle(hitResult, GetPawn()->GetActorLocation(), hitLocation, ECollisionChannel::ECC_PhysicsBody, FName());
+	//hitLocation = hitResult.Location;
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D screenLocation, FVector& lookDirection) const {
+	FVector worldLocation; // temp variable, discarded
+	return DeprojectScreenPositionToWorld(
+		screenLocation.X,
+		screenLocation.Y,
+		worldLocation,
+		lookDirection
+	);
+}
+
+//bool ATankPlayerController::GetLookVectorHitLocation(FVector start, FVector end, FHitResult& outHitResult) const {
+bool ATankPlayerController::GetLookVectorHitLocation(FVector lookDirection, FVector& outHitLocation) const {
 	FHitResult hitResult;
-	FVector start;
-	bool bHitActor;
-	bHitActor = GetWorld()->GetFirstPlayerController()->ActorLineTraceSingle(hitResult, GetPawn()->GetActorLocation(), hitLocation, ECollisionChannel::ECC_PhysicsBody, FName());
-	hitLocation = hitResult.Location;
-	return bHitActor;
+	FVector start = PlayerCameraManager->GetCameraLocation();
+	bool bGotHit = GetWorld()->LineTraceSingleByChannel(
+		hitResult,
+		start,
+		start + (lookDirection * lineTraceRange),
+		ECollisionChannel::ECC_Visibility
+	);
+	if (bGotHit) {
+		outHitLocation = hitResult.Location;
+	}
+	else {
+		outHitLocation = FVector(0);
+	}
+	return bGotHit;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -19,10 +19,23 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+private:
 	ATank* GetControlledTank() const;
 
 	// start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
 	bool GetSightRayHitLocation(FVector& hitLocation) const;
+	bool GetLookDirection(FVector2D screenLocation, FVector& lookDirection) const;
+	//bool GetLookVectorHitLocation(FVector start, FVector end, FHitResult& outHitResult) const;
+	bool GetLookVectorHitLocation(FVector lookDirection, FVector& outHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float crosshairLocationX = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float crosshairLocationY = 0.33333f;
+
+	UPROPERTY(EditAnywhere)
+	float lineTraceRange = 1000000.f; // 10 km
 };
